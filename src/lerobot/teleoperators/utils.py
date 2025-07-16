2170a9d4ec728c6a17d965437283f384a9d9904b
S.Satoya <c0b230860d@edu.teu.ac.jp>
2025-07-16 13:48:58 +0900

    feat(teleoperators): Add SO101 Dual Leader robot config

--- src/lerobot/teleoperators/utils.py
+++ src/lerobot/teleoperators/utils.py
@@ -29,6 +29,10 @@ def make_teleoperator_from_config(config: TeleoperatorConfig) -> Teleoperator:
         from .so100_leader import SO100Leader
 
         return SO100Leader(config)
+    elif config.type == "so101_dual_leader":
+        from .so101_dual_leader import SO101DualLeader
+
+        return SO101DualLeader(config)
     elif config.type == "so101_leader":
         from .so101_leader import SO101Leader
 
